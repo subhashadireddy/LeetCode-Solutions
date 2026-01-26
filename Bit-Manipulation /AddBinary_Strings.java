b2cba148a28ab262fc58e5b82dd055adf8cc5b64
subhashadireddy <subhashadireddy@gmail.com>
2026-01-26 18:20:08 +0530

    Create AddBinary_Strings.java
    67
    after the sum is calculated the the string is reversed ,, we use string builder to append the string

---
+++ Bit-Manipulation /AddBinary_Strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i>=0 || j>=0 || carry==1)
+        {
+            // int sum = carry;
+            if(i>=0)
+            {
+                carry += a.charAt(i) - '0';
+                i--;
+            }
+            if(j>=0)
+            {
+                carry += b.charAt(j) - '0';
+                j--;
+            }
+            sb.append(carry%2);
+            carry /= 2;
+        }
+        return sb.reverse().toString();
+    }
+}
