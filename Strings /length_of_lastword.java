ef56763f6ce731d15d975d4f56ca4eecba3f9c46
subhashadireddy <subhashadireddy@gmail.com>
2026-01-30 20:10:14 +0530

    Implement lengthOfLastWord method in Solution class
    by using trim we can removing the trailing spaces over there, and traversL THE string from reverse and when ever the white space occur t, then break and return the count ..

---
+++ Strings /length_of_lastword.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int c=0;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            if(str.charAt(i)!=' ')
+            {
+                c++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return c;
+    }
+}
