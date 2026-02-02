c853d5d47ee50c79307ede175a209be10b976a77
subhashadireddy <subhashadireddy@gmail.com>
2026-02-02 20:34:34 +0530

    Implement maxProfit method to calculate stock profit

---
+++ Arrays/besttimetosellstocks_sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int p=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(buy > prices[i]){
+                buy = prices[i];
+            }
+            else if(prices[i] - buy > p)
+            {
+                p=prices[i] - buy;
+            }
+        }
+        return p;
+    }
+}
