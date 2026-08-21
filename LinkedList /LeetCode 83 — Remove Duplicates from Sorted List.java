e84fccfdba9bbbb19f6e20bfc765c65b8d393270
ADIREDDY VENKATA SUBHASH <subhashadireddy@gmail.com>
2026-08-21 22:31:38 +0530

    Add method to delete duplicates in sorted linked list

---
+++ LinkedList /LeetCode 83 — Remove Duplicates from Sorted List.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+
+            if (current.val == current.next.val) {
+
+                // Skip duplicate
+                current.next = current.next.next;
+
+            } else {
+
+                // Move forward
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
